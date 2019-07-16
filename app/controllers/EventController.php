f8c9d6f060315601630c412cda5b8734a1e4543e
Zhukov <zavix1988@gmail.com>
2019-07-16 21:17:21 +0300

    events

--- app/controllers/EventController.php
+++ app/controllers/EventController.php
@@ -6,7 +6,7 @@ use core\base\Controller;
 use core\CommonHelper;
 use app\models\Events;
 
-class ClassName extends Controller
+class EventController extends Controller
 {
     private $events;
 
@@ -14,5 +14,20 @@ class ClassName extends Controller
     {
         parent::__construct($route);
         $this->events = new Events();
+    }
+
+    public function getRoomEvents($params)
+    {
+        $room = $params[0];
+        $result = $this->events->getEvents($room);
+        foreach($result as $event){
+            $user_id = $event['user_id'];
+            $room_id = $event['room_id'];
+            $day = substr($event['start_time'], 8, 2);
+            $start_time = substr($event['start_time'], 11, 5);
+            $end_time = substr($event['end_time'], 11, 5);
+
+        }
+        $this->setData([$end_time]);
     }
 }
