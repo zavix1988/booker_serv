b7bab112978353d7bdd391a85655a4a33ac1865e
Zhukov <zavix1988@gmail.com>
2019-07-16 21:17:32 +0300

    events

--- app/models/Events.php
+++ app/models/Events.php
@@ -6,4 +6,17 @@ use core\base\Model;
 class Events extends Model
 {
     protected $table = 'events';
+
+
+    //Create
+    //Read
+
+    public function getEvents($room)
+    {
+        $sql = "SELECT * FROM {$this->table} WHERE room_id = ? AND start_time > '20190701' AND start_time < '20190801'";
+        return $this->pdo->query($sql, [$room]);
+    }
+
+    //Update
+    //Delete
 }
