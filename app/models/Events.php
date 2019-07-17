37e6b8ddfe394cfb945d0bc0ccfca153705a45f3
zavix <zavix1988@gmail.com>
2019-07-17 20:34:24 +0300

    gsdfg

--- app/models/Events.php
+++ app/models/Events.php
@@ -10,9 +10,16 @@ class Events extends Model
 
     //Create
 
-    public function createEvent($userId, $roomId, $note, $dateStart, $dateEnd, $dateCreated)
+    public function createEvent($user, $room, $note, $dateStart, $dateEnd, $dateCreated)
     {
+        $start = date("Y-m-d G:i:s", $dateStart);
+        $end = date("Y-m-d G:i:s", $dateEnd);
+        $created = date("Y-m-d G:i:s", $dateCreated);
+
+        $sql = "INSERT INTO {$this->table} (user_id, room_id, create_time, start_time, end_time, note, recurrent_event_id) 
+                        VALUES ?, ?, ?, ?, ?, ?, ?";
         
+        return $this->pdo->execute($sql, [$user, $room, $created, $start, $end, $note, 0]);        
     }
     
     //Read
@@ -21,7 +28,7 @@ class Events extends Model
     {
         $sql = "SELECT id, user_id, room_id, note, UNIX_TIMESTAMP(start_time) as startEvent, UNIX_TIMESTAMP(end_time) as endEvent, UNIX_TIMESTAMP(create_time) as createdEvent
                         FROM booker_events
-                        WHERE boardroom_id = ? AND MONTH(start_time) = ? AND YEAR(start_time) = ?";
+                        WHERE room_id = ? AND MONTH(start_time) = ? AND YEAR(start_time) = ?";
         return $this->pdo->query($sql, [$room, $month, $year]);
     }
 
